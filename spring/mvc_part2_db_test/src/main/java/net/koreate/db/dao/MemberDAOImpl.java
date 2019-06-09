648da74131a1113280e120350076d3e4218721fd
Cutiler <50136588+cutiler@users.noreply.github.com>
2019-06-10 08:51:33 +0900

    20190607

---
+++ spring/mvc_part2_db_test/src/main/java/net/koreate/db/dao/MemberDAOImpl.java
@@ -0,0 +1,30 @@
+package net.koreate.db.dao;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import net.koreate.db.vo.MemberVO;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO{
+
+	@Inject
+	private SqlSession session;
+	
+	private String nameSpace = "net.koreate.mappers.MemberMapper";
+	
+	@Override
+	public void insertMember(MemberVO memberVO) {
+		System.out.println("insertmember : "+memberVO);
+		session.insert(nameSpace+".insertmember",memberVO);
+	}
+
+	@Override
+	public MemberVO readMember(String userId) {
+
+		return null;
+	}
+
+}
